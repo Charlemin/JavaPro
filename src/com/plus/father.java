cfab73b515d6a04131285ee2e1794340882f4012
Charlemin <1835450808@qq.com>
2016-04-29 10:54:43 +0800

    edit

--- src/com/plus/father.java
+++ src/com/plus/father.java
@@ -3,12 +3,21 @@ package com.plus;
 /**
  * Created by Administrator on 2016/4/8.
  */
-public class father{
-    public father(String str){
+public class father extends oppa {
+    public father(String str) {
+
+        System.out.println(str);
+    }
+
+    public void print(){
 
         System.out.println("father");
     }
+    public static void main(String[] args) {
 
+        new father("hello").print();
+    }
+}
     class oppa{
 
         public void print(){
@@ -16,6 +25,4 @@ public class father{
             System.out.println("oppa");
         }
     }
-
-}
 
