415c86cefa8a0381b3f10414116b28e2fd53bce3
Charlemin <Charelemin@github.com>
2016-08-13 11:16:34 +0800

    8-6

--- src/main/java/com/algorithm/Fibonacci.java
+++ src/main/java/com/algorithm/Fibonacci.java
@@ -43,3 +43,85 @@ public class Fibonacci {
             }
          }
     }
+
+/**
+ #include <iostream>
+ #include <vector>
+ #include <algorithm>
+ #include <map>
+ #include <math.h>
+ using namespace std;
+
+ // 深搜
+ void dfs(map<int,int> & mp, map<int,int>::iterator i,int curUseCnt, int & cnt, int curSum, int curPro)
+ {
+ //cout << "cur sum " << curSum << "\t" << "curPro:" << curPro << endl;
+ if (curSum > curPro)
+ {
+ //cout << "enter the loop " <<curSum <<"\t"<< curPro<< endl;
+ //cout << "i first:" << i->first << "curUseCnt: " << curUseCnt<< endl;
+ cnt ++;
+ //return ;
+ }
+ if (i == mp.end())
+ {
+ //cout << "mp end " << endl;
+ return;
+ }
+
+ for(map<int,int> ::iterator j = (++i);j!=mp.end();j++)
+ {
+ for(int k = 0; k <= j ->second;k++)
+ {
+ //cout << "j ->first" << j->first << "\t" << "k:" << k << endl;
+ dfs(mp,j,k,cnt,curSum + (k * j->first), curPro * (pow(j->first,k)));
+ }
+
+ }
+
+
+ }
+
+ int main() {
+ //out << "Hello, World!" << endl;
+
+ int n;
+ while (cin >> n)
+ {
+ //vector<int> v (n,0);
+ map<int,int> mp;
+ for(int i=0; i< n;i++)
+ {
+ int t;
+ cin >> t;
+ if (mp.find(t) == mp.end())
+ {
+ mp[t] = 1;
+ }
+ else
+ {
+ mp[t] ++;
+ }
+ }
+ int cnt = 0;
+ //        dfs(mp,mp.begin(),0,cnt,0,1);
+ map<int,int>::iterator i = mp.begin();
+ for (int k =0; k <= i->second;k++)
+ {
+
+ dfs(mp, i, k, cnt, k * (i->first), pow(i->first, k));
+ //cout << "cnt" << cnt << endl;
+ }
+ //sort(v.begin(),v.end());
+ cout << cnt << endl;
+
+
+
+
+ }
+
+ return 0;
+ }
+
+
+ */
