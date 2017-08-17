d51a671cde8880fe8bc7286e82e0999379ab774f
zhamin <zhamin@xiaomi.com>
2017-08-17 15:22:31 +0800

    zhamin

--- src/main/java/com/concurrent/ConcurrentNavigableMapTest.java
+++ src/main/java/com/concurrent/ConcurrentNavigableMapTest.java
@@ -2,13 +2,6 @@ package com.concurrent;
 
 public class ConcurrentNavigableMapTest {
     public static void main(String[] args) {
-//        ConcurrentNavigableMap map = new ConcurrentSkipListMap();
-//        map.put("1","1");
-//        map.put("2","2");
-//        map.put("3","3");
-//        map.put("4","4");
-//        ConcurrentNavigableMap headMap = map.headMap("2");
-//        System.out.println(headMap);
         int i = 0;
         if((i = 7 - 5) == 2)
             System.out.println(i);
