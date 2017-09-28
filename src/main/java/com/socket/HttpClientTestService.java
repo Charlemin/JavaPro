ab17e0f107165488dff68e76175e3b50969dec07
zhamin <zhamin@xiaomi.com>
2017-09-28 15:24:59 +0800

    zhamin

--- src/main/java/com/socket/HttpClientTestService.java
+++ src/main/java/com/socket/HttpClientTestService.java
@@ -1,49 +1,68 @@
 package com.socket;
 
 import net.sf.json.JSONObject;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class HttpClientTestService {
 
     public static void main(String[] args) {
 
-        System.out.println(new HttpClientTestService().getRequest("zhamin"));
-        System.out.println(new HttpClientTestService().postRequest("http://god-staging.d.pt.xiaomi.com/tmc/usertaggroupmap/add","{\"groupName\":\"push\",\"tagName\":\"xmmall_oversea\"}"));
-
+        System.out.println(new HttpClientTestService().getTest("http://god-staging.d.pt.xiaomi.com/v2/profile/stars"));
+        //System.out.println(new HttpClientTestService().postRequest("http://god-staging.d.pt.xiaomi.com/tmc/usertaggroupmap/add","{\"groupName\":\"unique\",\"tagName\":\"xmmll_oversea\"}"));
+        System.out.println(new HttpClientTestService().postTest("http://god-staging.d.pt.xiaomi.com/tmc/usertaggroupmap/add","{\"groupName\":\"unique\",\"tagName\":\"xmmll_oversea\"}"));
     }
 
-    public JSONObject getRequest(String request) {
-
-        try {
-            //String url = "http://god-staging.d.pt.xiaomi.com/tmc/ids/" + userName;
-            String url = "http://god-staging.d.pt.xiaomi.com/v2/profile/stars";
-            HttpClient client = new HttpClient();
-            HttpMethod getMethod = new GetMethod(url);
-            getMethod.getParams().setParameter(HttpMethodParams.HTTP_CONTENT_CHARSET, "UTF-8");
-            client.executeMethod(getMethod);
-            String responseString = getMethod.getResponseBodyAsString();
-            return JSONObject.fromObject(responseString);
-        } catch (Exception ignore) {
-            System.out.println(ignore.getMessage());
+    public JSONObject getTest(String url){
+        CloseableHttpClient client = HttpClients.createDefault();
+        CloseableHttpResponse response;
+        try{
+            HttpGet httpGet = new HttpGet(url);
+            response = client.execute(httpGet);
+            System.out.println(response.getStatusLine().getStatusCode());
+            HttpEntity entity = response.getEntity();
+            Header[] headers = response.getAllHeaders();
+            for (Header header : headers){
+                System.out.println(header.getName() + ":" + header.getValue());
+            }
+            if (entity != null)
+                return JSONObject.fromObject(EntityUtils.toString(entity, "UTF-8"));
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
         return null;
     }
 
-    public JSONObject postRequest(String url,String request){
-        HttpClient httpClient = new HttpClient();
-        PostMethod postMethod = new PostMethod(url);
-        NameValuePair para = new NameValuePair("data",request);
+    public JSONObject postTest(String url,String request){
+        CloseableHttpClient client = HttpClients.createDefault();
+        CloseableHttpResponse response;
         try{
-            postMethod.setRequestBody(new NameValuePair[]{para});
-            int status = httpClient.executeMethod(postMethod);
-            System.out.println(status);
-            return JSONObject.fromObject(postMethod.getResponseBodyAsString());
+            HttpPost httpPost = new HttpPost(url);
+            List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+            formparams.add(new BasicNameValuePair("data", request));
+            UrlEncodedFormEntity uefEntity = new UrlEncodedFormEntity(formparams, "UTF-8");
+            httpPost.setEntity(uefEntity);
+            response = client.execute(httpPost);
+            System.out.println(response.getStatusLine().getStatusCode());
+            Header[] headers = response.getAllHeaders();
+            for (Header header : headers){
+                System.out.println(header.getName() + ":" + header.getValue());
+            }
+            HttpEntity entity = response.getEntity();
+            if (entity != null)
+                return JSONObject.fromObject(EntityUtils.toString(entity, "UTF-8"));
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
